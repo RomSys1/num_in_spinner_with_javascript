6bb74142fd8e103c4b1feb6001cdf7c3fb95de3f
Roman Franco <romanfrancovc@gmail.com>
2021-01-06 01:34:54 -0400

    funcionando boton

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# num_in_spinner_with_javascript
+# Number Input Spinner
+
 ui design
+
+<p align="center">
+    <kbd>
+        <img src="./images/vista1.jpg" alt="Demo"/>
+->
+        <img src="./images/vista2.jpg" alt="Demo"/>
+->
+        <img src="./images/vista3.jpg" alt="Demo"/>
+->
+        <img src="./images/vista4.jpg" alt="Demo"/>
+    </kbd>
+</p>
--- script.js
+++ script.js
@@ -1,21 +1,32 @@
+/*selecionamos al que sera el padre del texto en el boton*/
+var numbers = document.getElementById("box");
 
-
-    var numbers = document.getElementById("box");
-    for (i = 0; i < 100; i++) {
+/*creamos texto dentro de boton desde 1 a 99*/
+for (i = 0; i < 100; i++) {
     var span = document.createElement("span");
-        span.textContent = i;
-        numbers.appendChild(span);
-    }
+    span.textContent = i;
+    numbers.appendChild(span);
+    /*0*/
+    /*01*/
+    /*012*/
+    /*.........*/
+}
 
-    var num = numbers.getElementsByTagName("span");
-    var index = 0;
-    function nextNum() {
-        num[index].style.display = "none";
-        index = (index + 1) % num.length;
-        num[index].style.display = "initial";
-    }
-    function prevNum() {
-        num[index].style.display = "none";
-        index = (index - 1 + num.length) % num.length;
-        num[index].style.display = "initial";
-    }
+var num = numbers.getElementsByTagName("span");
+var index = 0;
+
+function nextNum() {
+    /*oculta el anterior child*/
+    num[index].style.display = "none";
+    index = (index + 1) % num.length;
+    /*visible el actual child*/
+    num[index].style.display = "initial";
+}
+
+function prevNum() {
+    /*oculta el actual child*/
+    num[index].style.display = "none";
+    index = (index - 1 + num.length) % num.length;
+    /*visible el anterior child*/
+    num[index].style.display = "initial";
+}
